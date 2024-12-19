25e698b092282cbda541251eeaffc95d17bf2ae0
Ritika Gautam <ritika100898@gmail.com>
2024-12-19 12:12:07 +0530

    Mobile Changes

--- packages/MobileApp/App.tsx
+++ packages/MobileApp/App.tsx
@@ -1,76 +1,317 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  FlatList,
+  Image,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  View,
 } from 'react-native';
-import _ from 'lodash';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Logo from './src/images/logo.png';
+import Custom from './src/images/custom1.jpg';
+import Header1 from './src/images/header1.jpg';
+import Header2 from './src/images/header2.jpg';
+import Header3 from './src/images/header3.jpg';
+import Project1 from './src/images/project1.jpg';
+import Project2 from './src/images/project2.jpg';
+import Project3 from './src/images/project3.jpg';
+import Project4 from './src/images/project4.jpg';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  const data = [
-    {id: 1, name: 'John', age: 25},
-    {id: 2, name: 'Jane', age: 22},
-    {id: 3, name: 'Alice', age: 30},
-    {id: 4, name: 'Bob', age: 22},
-  ];
-  const filteredData = _.filter(data, person => person.age > 22);
-  const sortedData = _.sortBy(filteredData, 'age');
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentSlide(prevSlide => (prevSlide + 1) % slidingDATA.length);
+    }, 10000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(slideInterval);
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <Text style={styles.header}>Filtered and Sorted List</Text>
-      <FlatList
-        data={sortedData}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => (
-          <Text style={styles.item}>
-            {item.name} - Age: {item.age}
-          </Text>
-        )}
-      />
+    <SafeAreaView style={styles.mainContainer}>
+      <StatusBar />
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.firstView}>
+            <ScrollView horizontal>
+              <View style={styles.logoView}>
+                <Image source={Logo} style={styles.logo} />
+              </View>
+              {tabsDATA?.map(item => {
+                return (
+                  <View style={styles.tabView}>
+                    <Text>{item}</Text>
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </View>
+          <View style={styles.secondView}>
+            <View style={styles.slidingImageView}>
+              <Image
+                source={slidingDATA[currentSlide]?.image}
+                style={styles.slidingImage}
+                resizeMode={'stretch'}
+              />
+            </View>
+            <View style={styles.slidingTextView}>
+              <Text>{slidingDATA[currentSlide]?.heading}</Text>
+              <Text>{slidingDATA[currentSlide]?.details}</Text>
+            </View>
+          </View>
+          <View>
+            <ScrollView horizontal style={styles.thirdView}>
+              {thirdSectionDATA?.map(item => {
+                return (
+                  <View
+                    style={[
+                      styles.thirdViewItem,
+                      {backgroundColor: item.color},
+                    ]}>
+                    <Text style={styles.fourthText}>{item?.heading}</Text>
+                    <Text style={styles.fourthText}>{item?.description}</Text>
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </View>
+
+          <View>
+            <Text>Latest Projects</Text>
+            <ScrollView horizontal>
+              {latestProjectDATA?.map(item => {
+                return (
+                  <View style={styles.fourthViewItem}>
+                    <Image
+                      source={item.image}
+                      style={{width: 200, height: 200}}
+                    />
+                    <View style={styles.fourthTextView}>
+                      <Text style={styles.fourthText}>{item?.description}</Text>
+                    </View>
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </View>
+
+          <View>
+            <View style={styles.fifthView}>
+              <View style={styles.fifthImageView}>
+                <Image source={Custom} style={styles.fifthImage} />
+              </View>
+              <View style={styles.fifthViewText}>
+                <Text>JUST FEW WORDS</Text>
+                <Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corrupti, aut.jhbjk wnkjndkml kmdmdlkmakmdla jndlqndlkn
+                  dlkndlkw jwndlwndln
+                </Text>
+                <Text>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.bottomView}>
+            <Text>Popular Articles</Text>
+            <Text>Subpage</Text>
+            <Text>Error 404</Text>
+            <Text>Example</Text>
+            <Text>Typography</Text>
+            <Text>All Template Positions</Text>
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#e7e7e7',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    padding: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  logoView: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  logo: {
+    width: '100%',
+    height: '100%',
   },
-  header: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 16,
+  tabView: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: 'red',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
-  item: {
-    fontSize: 16,
-    marginVertical: 4,
+  firstView: {
+    paddingBottom: 20,
+  },
+  secondView: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+  slidingImageView: {
+    width: '30%',
+    height: 120,
+  },
+  slidingImage: {
+    width: '100%',
+    height: '100%',
+  },
+  slidingTextView: {
+    width: '70%',
+    padding: 10,
+  },
+  thirdView: {
+    flexDirection: 'row',
+  },
+  thirdViewItem: {
+    width: 200,
+    height: 120,
+    padding: 10,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  fourthViewItem: {
+    width: 200,
+    marginRight: 20,
+  },
+  fourthTextView: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 10,
+  },
+  fourthText: {
+    color: 'white',
+  },
+  fifthView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  fifthImageView: {width: '40%', height: 150},
+  fifthImage: {
+    width: '100%',
+    height: '100%',
+  },
+  fifthViewText: {
+    width: '60%',
+    padding: 10,
+  },
+  bottomView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    backgroundColor: 'white',
+    marginTop: 20,
   },
 });
 
 export default App;
+const tabsDATA = [
+  'HOME',
+  'FEATURES',
+  'EXAMPLES',
+  'PAGES',
+  'LANGUAGES',
+  'CONTACT US',
+];
+const slidingDATA = [
+  {
+    name: 'image1',
+    image: Header1,
+    heading: 'STARTING YOUR OWN BUSINESS',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde neque fugiat  provident possimus et atque culpa vitae minus repellendus autem?',
+  },
+  {
+    name: 'image2',
+    image: Header2,
+    heading: 'MICROSOFT OFFICE TRAINING',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde neque fugiat  provident possimus et atque culpa vitae minus repellendus autem?',
+  },
+  {
+    name: 'image3',
+    image: Header3,
+    heading: 'ANDROID DEVELOPMENT',
+    details:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde neque fugiat  provident possimus et atque culpa vitae minus repellendus autem?',
+  },
+];
+
+const thirdSectionDATA = [
+  {
+    heading: 'TIME MACHINE',
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+    color: '#017eba',
+    class: 'fa-solid fa-clock fa-6x',
+  },
+  {
+    heading: 'ANALYTICS',
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+    color: '#a0b046',
+    class: 'fa-solid fa-tv fa-6x',
+  },
+  {
+    heading: 'WORLDWIDE SERVICES',
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+    color: '#f78145',
+    class: 'fa-solid fa-earth-asia fa-6x',
+  },
+];
+
+const latestProjectDATA = [
+  {
+    name: 'Project1',
+    image: Project1,
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+  },
+  {
+    name: 'Project2',
+    image: Project2,
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+  },
+  {
+    name: 'Project3',
+    image: Project3,
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+  },
+  {
+    name: 'Project4',
+    image: Project4,
+    description:
+      'Some quick example text to build on the card title and make up the bulk of the cards content.',
+  },
+];
